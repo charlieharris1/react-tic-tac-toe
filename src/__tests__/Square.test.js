7be09bb426476315629ef62bc5ed40d17fbbae5c
Charlie Harris <ceharris92@gmail.com>
2019-02-02 16:27:37 +0000

    Add empty tests for exercise

---
+++ src/__tests__/Square.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+describe("Square", () => {
+  test("displays the value passed in", () => {
+  });
+
+  test("sets the correct class", () => {
+  });
+
+
+  test("calls the on click function when clicked", () => {
+  });
+
+});
