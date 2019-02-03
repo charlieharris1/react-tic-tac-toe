aa0948027352bb8cfc796069a9575b4b3ba3c9b2
Charlie Harris <ceharris92@gmail.com>
2019-02-03 12:07:59 +0000

    Update enzyme tests to use mount

--- src/__tests__/Square.test.js
+++ src/__tests__/Square.test.js
@@ -1,29 +1,28 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
 import { Square } from "../Square";
 
 describe("Square", () => {
   test("displays the value passed in", () => {
-    const square = shallow(<Square value="X" onClick={() => {}} />);
+    const square = mount(<Square value="X" onClick={() => {}} />);
 
-    expect(square.text()).toEqual("X");
+    expect(square.find('button').text()).toEqual("X");
   });
 
   test("sets the correct class", () => {
-    const square = shallow(<Square value="X" onClick={() => {}} />);
+    const square = mount(<Square value="X" onClick={() => {}} />);
 
-    expect(square.prop('className')).toEqual("square");
+    expect(square.find('button').hasClass("square")).toEqual(true);
   });
 
 
   test("calls the on click function when clicked", () => {
     const onClick = jest.fn();
-    const square = shallow(<Square value="X" onClick={onClick} />);
+    const square = mount(<Square value="X" onClick={onClick} />);
 
     square.simulate('click');
 
     expect(onClick).toHaveBeenCalled();
   });
-
 });
